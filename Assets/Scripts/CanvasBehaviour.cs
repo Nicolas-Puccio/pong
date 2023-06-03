c966fdeba66c3bcf751bf13bb102ddf8cb8b2ea8
Puccio Nicolas <puccio.nicolas.98@gmail.com>
2023-06-03 11:50:04 +0700

    fix bug in CanvasBehaviour, and add debug keybinds

--- Assets/Scripts/CanvasBehaviour.cs
+++ Assets/Scripts/CanvasBehaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode;
 
 public class CanvasBehaviour : MonoBehaviour
 {
@@ -39,8 +40,11 @@ public class CanvasBehaviour : MonoBehaviour
         int offset = Screen.height/2-25;
         foreach (RectTransform son in transform.GetComponentsInChildren<Transform>())
         {
-            string pos = son.name.Substring(5);
-            son.anchoredPosition = pos == "top" ? new Vector2(0,offset) : pos == "right" ? new Vector2(offset,0) : pos == "bot" ? new Vector2(0,-offset) : new Vector2(-offset,0);
+            if(son.name.StartsWith("score"))
+            {
+                string pos = son.name.Substring(5);
+                son.anchoredPosition = pos == "top" ? new Vector2(0,offset) : pos == "right" ? new Vector2(offset,0) : pos == "bot" ? new Vector2(0,-offset) : new Vector2(-offset,0);
+            }
         }
     }
 
@@ -54,5 +58,20 @@ public class CanvasBehaviour : MonoBehaviour
     //only on server
     public void EnableButton(){
         startButton.SetActive(true);    
+    }
+
+
+
+
+    //-debug only
+    public void Update(){
+        if (Input.GetKeyDown(KeyCode.H))
+        {  
+             NetworkManager.Singleton.StartHost();     
+        }
+         if (Input.GetKeyDown(KeyCode.C))
+        {    
+            NetworkManager.Singleton.StartClient();
+        }
     }
 }
