0c52bb22ea89ec702a702945e0c2511cb9bf6a9a
Puccio Nicolas <puccio.nicolas.98@gmail.com>
2023-06-03 09:55:16 +0700

    tweak player starting position remove old code

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -7,7 +7,7 @@ public class GameMode :  NetworkBehaviour
 {
     public int clientsRequired;
 
-    string[] positions = new string[] { "top", "right", "bot", "left" };
+    string[] positions = new string[] { "top",  "bot","right", "left" };
     int nextPos =  0; //index de la posicion del proximo jugador
 
     public override void OnNetworkSpawn()
@@ -25,48 +25,7 @@ public class GameMode :  NetworkBehaviour
         return positions[nextPos++];
     }
     
-    void Start()
-    {/*
-        float cameraSize = Camera.main.orthographicSize;
-        float backgroundSize = cameraSize*2 * mapRatio / 100;
-        GameObject.Find("Background").transform.localScale = new Vector2(backgroundSize,backgroundSize);
     
-        //distance from center
-        float playerSpawnDistance = backgroundSize/2-playerOffsetFromEdge;
-        
-        // Load the prefab from the "Resources" folder
-        
-        GameObject playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
-         if (playerPrefab == null)
-        {
-            Debug.LogError("Prefabs/Player prefab missing");
-            return;
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            string stringPosition = i == 0 ? "top" : i == 1 ? "right" : i == 2 ? "bot" : "left";
-            Vector2 transformPosition = i == 0 ? new Vector2(0,playerSpawnDistance) : i == 1 ? new Vector2(playerSpawnDistance,0) : i == 2 ? new Vector2(0,-playerSpawnDistance) : new Vector2(-playerSpawnDistance,0);
-            Quaternion transformRotation = i==0||i==2?Quaternion.Euler(0f, 0f, 90f):Quaternion.identity;
-            
-            GameObject player = Instantiate(playerPrefab, transformPosition,transformRotation );
-            PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
-            if (playerMovement == null)
-            {
-                Debug.LogError("YourScriptOnInstantiatedObject component not found on the instantiated object.");
-                return;
-            }
-            
-                playerMovement.position = stringPosition;
-            
-            
-        }*/
-
-
-
-
-    }
-
     private static GameMode singleton;
 
     // Public property to access the instance
