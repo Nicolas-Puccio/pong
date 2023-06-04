7dfe69ab2233a866a3a404ad76ef680b6b54ec66
Puccio Nicolas <puccio.nicolas.98@gmail.com>
2023-06-04 18:14:09 +0700

    add wacky pickable and ball behaviour

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -13,18 +13,24 @@ public class BallMovement : NetworkBehaviour
 
   Rigidbody2D rb;
 
+
+  #region powerups data
+
+  public int wackyChance = 0;
+  public bool canWack = true;//set to false when it does, set to true oncollisionenter
+
+  #endregion
+
+
   void Awake()
   {
     rb = GetComponent<Rigidbody2D>();
   }
-  
+
 
   public void Start()
   {
-    float randomAngle = Random.Range(0f, 360f);
-
-    // Convert the angle to a direction vector
-    Vector2 direction = new Vector2(Mathf.Cos(randomAngle * Mathf.Deg2Rad), Mathf.Sin(randomAngle * Mathf.Deg2Rad));
+    Vector2 direction = RotationHelper.DegreesToVector2(Random.Range(0f, 360f));
 
     // Set the velocity with the constant speed and random direction
     rb.velocity = direction * speed;
@@ -35,7 +41,11 @@ public class BallMovement : NetworkBehaviour
   void OnCollisionEnter2D(Collision2D col)
   {
     if (col.gameObject.CompareTag("Player"))
+    {
       lastPlayerHit = col.gameObject;
+      canWack = true;
+    }
+
 
     //idk why balls lose velocity when colliding against each other despite both being bouncy, so i set a lower limit to magnitude
     if (rb.velocity.magnitude < speed)
@@ -49,6 +59,34 @@ public class BallMovement : NetworkBehaviour
 
   void FixedUpdate()
   {
+    if (wackyChance != 0 && canWack)
+    {
+      //si esta cerca de la pared derecha o izquierda
+      if ((rb.velocity.x > 0 && transform.localPosition.x > BackgroundSize.backgroundSize / 3 && GameObject.Find("wallright") == null) ||
+      (rb.velocity.x < 0 && transform.localPosition.x < -BackgroundSize.backgroundSize / 3 && GameObject.Find("wallleft") == null))
+      {
+        if (Random.Range(0, 100) <= wackyChance)
+        {
+          rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+          canWack = false;
+        }
+      }
+
+
+      //arriba o abajo
+      if ((rb.velocity.y > 0 && transform.localPosition.y > BackgroundSize.backgroundSize / 3) ||
+      (rb.velocity.y < 0 && transform.localPosition.y < -BackgroundSize.backgroundSize / 3))
+      {
+        if (Random.Range(0, 100) <= wackyChance)
+        {
+          rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+          canWack = false;
+        }
+      }
+    }
+
+
+
     //-coudl check if it was close to hitting another wall and count towards both players
     if (transform.localPosition.x > BackgroundSize.backgroundSize / 2)
     {
--- Assets/Scripts/Pickable.cs
+++ Assets/Scripts/Pickable.cs
@@ -1,113 +1,137 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using Unity.Netcode;
 
 public class Pickable : MonoBehaviour
 {
-    //-deberia usar herencia
-    [SerializeField]
-    public MultiplyProperties multiplyProperties;
-    [SerializeField]
-    public ChangeBallProperties changeBallProperties;
-    [SerializeField]
-    public ShockSelfProperties shockSelfProperties;
+  //-deberia usar herencia
+  public MultiplyProperties multiplyProperties;
+  public ShockProperties shockProperties;
+  public WackyBallProperties wackyBallProperties;
 
-    static GameObject  ballPrefab;
+  static GameObject ballPrefab;
 
 
-    void Awake()
+  void Awake()
+  {
+    if (!ballPrefab)
     {
-        if(!ballPrefab)
-            ballPrefab = ballPrefab = Resources.Load<GameObject>("Ball");
-
-
-        var random = UnityEngine.Random.Range(0, 1);
-
-        switch (random)
-        {
-            case 0:
-                // Handle Multiply powerup
-                multiplyProperties = new MultiplyProperties(UnityEngine.Random.Range(1, 5), UnityEngine.Random.Range(15, 30));
-                Debug.Log(multiplyProperties.amount);
-                Debug.Log(multiplyProperties.spreadDegrees);
-                break;
-            case 1:
-                // Handle ChangeBall powerup
-                break;
-            default:
-                // Handle unknown powerup type
-                Debug.Log("Unknown powerup type.");
-                break;
-        }
+      ballPrefab = ballPrefab = Resources.Load<GameObject>("Ball");
     }
 
-    //-for the enable checkbox to appear
-    void Start(){}
 
+    var random = UnityEngine.Random.Range(0, 3);
 
-    void OnTriggerEnter2D(Collider2D col)
+    switch (random)
     {
-        if(!col.gameObject.CompareTag("Ball"))
-            return;
-
-        //-disable self to avoid infinite loop do i really need this?
-        GetComponent<CircleCollider2D>().enabled = false;
-
-
-        if(multiplyProperties!=null){
-
-        for (int i = 0; i < multiplyProperties.amount; i++)
-        {
-    
-            Debug.Log("SPAWN BALL");
-            /*
-            Vector2 direction = (transform.position - col.transform.position).normalized;
-
-            int randomAngle = UnityEngine.Random.Range(-multiplyProperties.spreadDegrees, multiplyProperties.spreadDegrees);
-            Quaternion randomRotation = Quaternion.Euler(0, 0, randomAngle);
-            Vector2 randomDirection = randomRotation * direction;
-            */
-
-            GameObject ball = Instantiate(ballPrefab, transform);
-            ball.GetComponent<BallMovement>().enabled = true;
-            ball.GetComponent<BallMovement>().InitBallVelocity();
-            ball.GetComponent<NetworkObject>().Spawn();
-
-        }
-
-            //Destroy self
-            GetComponent<NetworkObject>().Despawn();
-            Destroy(gameObject);
-        }
+      case 0:
+        // Handle Multiply powerup
+        multiplyProperties = new MultiplyProperties(UnityEngine.Random.Range(1, 5));
+        GetComponent<SpriteRenderer>().color = new Color(0.3f, 1f, 0.6f, 1f);
+        break;
+      case 1:
+        // Handle Shock powerup
+        shockProperties = new ShockProperties(UnityEngine.Random.value > 0.8f);
+        GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 0f, 1f);
+        break;
+      case 2:
+        // Handle Wacky powerup
+        wackyBallProperties = new WackyBallProperties(100);
+        GetComponent<SpriteRenderer>().color = new Color(0f, 01f, 0f, 1f);
+        break;
+      default:
+        // Handle unknown powerup type
+        Debug.Log("Unknown powerup type.");
+        break;
     }
+  }
+
+  //-for the enable checkbox to appear
+  void Start() { }
+
+
+  void OnTriggerEnter2D(Collider2D col)
+  {
+    if (!col.gameObject.CompareTag("Ball"))
+      return;
+
+
+
+    //-disable self to avoid infinite loop do i really need this?
+    GetComponent<CircleCollider2D>().enabled = false;
+
+
+
+    if (multiplyProperties != null)
+    {
+      for (int i = 0; i < multiplyProperties.amount; i++)
+      {
+        Debug.Log("SPAWN BALL");
+        GameObject ball = Instantiate(ballPrefab, transform);
+        ball.GetComponent<BallMovement>().enabled = true;
+        ball.GetComponent<NetworkObject>().Spawn();
+      }
+
+      //Destroy self
+      GetComponent<NetworkObject>().Despawn();
+
+    }
+
+
+
+    if (shockProperties != null)
+    {
+      //-get pos of player
+      string pos = "";
+
+
+
+      GameMode.Singleton.Shock(pos);
+    }
+
+
+    if (wackyBallProperties != null)
+    {
+      col.GetComponent<BallMovement>().wackyChance = wackyBallProperties.steerChance;
+    }
+
+
+
+    Debug.Log("destroy pickable");
+    Destroy(gameObject);
+  }
 }
 
-public class MultiplyProperties {
-    //new amount? new velocity? new scale?
-    public int amount;
-    public int spreadDegrees;
-    
-    public MultiplyProperties(int amount, int spreadDegrees) {
-        this.amount = amount;
-        this.spreadDegrees = spreadDegrees;
-    }
-}
+public class MultiplyProperties
+{
+  public int amount; // amount of new balls spawned
 
-public class ChangeBallProperties {
-    //new scale? new mass? new texture?
-    //public int amount;
-    public ChangeBallProperties(int amount, int spreadDegrees) {
-       
-    }
+  public MultiplyProperties(int amount)
+  {
+    this.amount = amount;
+  }
 }
 
 
-public class ShockSelfProperties {
-    //new scale? new mass? new texture?
-    //public int amount;
-    public ShockSelfProperties(int amount, int spreadDegrees) {
-        
-    }
+public class ShockProperties
+{
+  public bool shockAll; //true shocks all players, false only player that hit it
+
+
+  public ShockProperties(bool shockAll)
+  {
+    this.shockAll = shockAll;
+  }
+}
+
+
+public class WackyBallProperties
+{
+  public int steerChance; //0-100 chance the ball stears
+
+  public WackyBallProperties(int steerChance)
+  {
+    this.steerChance = steerChance;
+  }
 }
